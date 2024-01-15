55cab150a4906158ad5669a57502ceb325872e43
dassimanuel000 <dassimanuel000@gmail.com>
2024-01-15 20:05:46 +0100

    API ROUTER

--- dictionary/dictionary.go
+++ dictionary/dictionary.go
@@ -1,59 +1,73 @@
 package dictionary
 
 import (
-    "fmt"
-    "os"
+	"errors"
+	"fmt"
+	"os"
+	"sync"
 )
 
 type Entry struct {
-}
-
-func (e Entry) String() string {
-
-	return ""
+	Definition string `json:"definition"`
 }
 
 type Dictionary struct {
 	entries map[string]Entry
+	lock    sync.RWMutex
 }
 
-func New() *Dictionary {
-
-	return nil
+func NewDictionary() *Dictionary {
+	return &Dictionary{
+		entries: make(map[string]Entry),
+	}
 }
 
 func (d *Dictionary) Add(word string, definition string) {
+	d.lock.Lock()
+	defer d.lock.Unlock()
 
+	d.entries[word] = Entry{Definition: definition}
 }
 
 func (d *Dictionary) Get(word string) (Entry, error) {
+	d.lock.RLock()
+	defer d.lock.RUnlock()
 
-	return Entry{}, nil
+	entry, ok := d.entries[word]
+	if !ok {
+		return Entry{}, errors.New("Mot non trouvé")
+	}
+
+	return entry, nil
 }
 
 func (d *Dictionary) Remove(word string) {
+	d.lock.Lock()
+	defer d.lock.Unlock()
 
+	delete(d.entries, word)
 }
 
-func (d *Dictionary) List() ([]string, map[string]Entry) {
+func (d *Dictionary) List() map[string]Entry {
+	d.lock.RLock()
+	defer d.lock.RUnlock()
 
-	return []string{}, d.entries
+	return d.entries
 }
 
 func (d *Dictionary) SavuverEnFichier(filename string) error {
-    file, err := os.Create(filename)
-    if err != nil {
-        return err
-    }
-    defer file.Close()
+	file, err := os.Create(filename)
+	if err != nil {
+		return err
+	}
+	defer file.Close()
 
-    for word, entry := range d.entries {
-        _, err := fmt.Fprintf(file, "%s: %s\n", word, entry.String())
-        if err != nil {
-            return err
-        }
-    }
-
-    return nil
+	/*for word, entry := range d.entries {
+	    _, err := fmt.Fprintf(file, "%s: %s\n", word, entry.string())
+	    if err != nil {
+	        return err
+	    }
+	}*/
+	fmt.Println("Succèssssssssssssss.")
+	return nil
 }
-
--- main.go
+++ main.go
@@ -1,44 +1,54 @@
 package main
 
 import (
-    "fmt"
-    "cours1/dictionary"
+	"cours1/dictionary"
+	"fmt"
+	"log"
+	"net/http"
+
+	"github.com/gorilla/mux"
 )
 
 func main() {
 
-    //*TEST POUR COMPRENDRE **/
-    fmt.Println("Hello, world.s			d")
+	//*TEST POUR COMPRENDRE **/
+	fmt.Println("Hello, world.s			d")
 
 	maMap := make(map[string]string)
-    maMap["article1"] = "VOIZL "
-    maMap["article2"] = " ERR"
+	maMap["article1"] = "VOIZL "
+	maMap["article2"] = " ERR"
 
 	for titre, description := range maMap {
-        fmt.Println("Titre :", titre)
-        fmt.Println("Description :", description)
-        fmt.Println()
-    }
+		fmt.Println("Titre :", titre)
+		fmt.Println("Description :", description)
+		fmt.Println()
+	}
 
-    fmt.Println("Carte   :", maMap)
+	fmt.Println("Carte   :", maMap)
 
-    //*FIN DU TEST POUR COMPRENDRE **/
+	//*FIN DU TEST POUR COMPRENDRE **/
 
-    dict := dictionary.New() 
-    dict.Add("mot1", "definition1")
-    dict.Add("mot2", "definition2")
+	dict := dictionary.NewDictionary()
+	dict.Add("mot1", "definition1")
+	dict.Add("mot2", "definition2")
 
-    err := dict.SavuverEnFichier("./dictionnaire.txt")
+	err := dict.SavuverEnFichier("./dictionnaire.txt")
 
-    if err != nil {
-        fmt.Println("Erreurrrrrrrr:", err)
-    } else {
-        fmt.Println("Succèssssssssssssss.")
-    }
+	if err != nil {
+		fmt.Println("Erreurrrrrrrr:", err)
+	} else {
+		fmt.Println("Succèssssssssssssss.")
+	}
 
 	if dict == nil {
 		fmt.Println("Dictionary  nul")
 	} else {
 		fmt.Println("Dictionary plein")
 	}
+
+	router := mux.NewRouter()
+	port := ":8080"
+
+	fmt.Printf("Serveur écoutant sur le port %s...\n", port)
+	log.Fatal(http.ListenAndServe(port, router))
 }
