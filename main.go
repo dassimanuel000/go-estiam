bcb9c09aaa482189861109e84eac283883df7d31
dassimanuel000 <dassimanuel000@gmail.com>
2024-01-18 12:57:56 +0100

    test gestion erreur

--- main.go
+++ main.go
@@ -51,4 +51,41 @@ func main() {
 
 	fmt.Printf("Serveur écoutant sur le port %s...\n", port)
 	log.Fatal(http.ListenAndServe(port, router))
+
+	router.HandleFunc("/api/{word}", GetDefinition).Methods("GET")
+
+}
+
+func GetDefinition(w http.ResponseWriter, r *http.Request) {
+
+	fmt.Fprintf(w, "Hello, World!")
+	params := mux.Vars(r)
+	word := params["word"]
+
+	w.WriteHeader(http.StatusOK)
+	fmt.Fprintf(w, "Mot : %s", word)
+
+	/*entry, err := dictionary.NewDictionary().Get(word)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusNotFound)
+		return
+	}
+
+	jsonResponse, _ := json.Marshal(entry)
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+	w.Write(jsonResponse)*/
+}
+
+func gestionnaireErreur(suivant http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		defer func() {
+			if err := recover(); err != nil {
+				log.Println("Erreur non capturée :", err)
+				http.Error(w, "Erreur interne du serveur", http.StatusInternalServerError)
+			}
+		}()
+
+		suivant.ServeHTTP(w, r)
+	})
 }
